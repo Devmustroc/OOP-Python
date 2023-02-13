7ef99a74772cf6ed49fa1f9cd5b627f9b7ff5923
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-02-13 13:43:58 +0100

    Uvicorn Fastapi pydantic baseModel

--- opp-class/FASTapi/main.py
+++ opp-class/FASTapi/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
@@ -8,18 +9,16 @@ app = FastAPI()
 class User(BaseModel):
     username: str
     short_description: str
-    liked_posts: list[int]
+    liked_posts: Optional[list[int]] = None
 
 
-def get_user_info() -> (str, str):
-    username = 'testuser'
-    short_description = 'This is a test user'
-    liked_posts = []
-    return username, short_description, liked_posts
+def get_user_info() -> User:
+    content = {"username": "testuser","short_description": "My shor bio"}
+    return User(**content)
+
 
 
 @app.get("/user/me",response_model=User)
 def test_endpoint():
-    username, short_description, liked_poste = get_user_info()
-    user = User(username=username, short_description=short_description, liked_poste=liked_posts)
+    user = get_user_info()
     return user
