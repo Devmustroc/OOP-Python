9205f36bef87a4a59120e7566bc69d69cd33834a
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-02-13 13:55:30 +0100

    Uvicorn Fastapi pydantic baseModel

--- opp-class/FASTapi/main.py
+++ opp-class/FASTapi/main.py
@@ -8,12 +8,12 @@ app = FastAPI()  # Create an instance of the FastAPI class
 
 class User(BaseModel):  # Create a class that inherits from the BaseModel class
     username: str  # The username is a string
-    short_description: str
-    liked_posts:  list[int] #Optional[list[int]] = None
+    short_description: str = "default value"
+    liked_posts: Optional[list[int]] = None  #list[int]  # The liked_posts is a list of integers
 
 
 def get_user_info() -> User: # The function returns a User object
-    content = {"username": "testuser","short_description": "My shor bio"} # Create a dictionary with the user information
+    content = {"username": "testuser", "short_description": "test description", "liked_posts": [1, 2, 3]}  # Create a dictionary
     return User(**content)  # Return the User object
 
 
