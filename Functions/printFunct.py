bcb5478b41e3259226188986a8fa035b39cb2118
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-03-08 11:02:16 +0100

    OOP & Python

---
+++ Functions/printFunct.py
@@ -0,0 +1,18 @@
+# Create Function in Python
+
+# def production(x,y):
+#     return x*y
+#
+# print(production(2,3))
+
+# def greet(name):
+#     print(f'Hello {name}')
+#
+# x = input('Enter your name: ');
+# greet(x)
+
+# def greet(name):
+#     print(f'Hello {name}')
+#
+# x = input('Enter your name: ');
+# greet(x)
