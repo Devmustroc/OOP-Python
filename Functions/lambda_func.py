d9dacac6f6e3b47e4dd9ad2f8021ec12a6cae058
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-03-08 12:05:09 +0100

    Function

--- Functions/lambda_func.py
+++ Functions/lambda_func.py
@@ -4,8 +4,19 @@
 
 # sub = lambda x, y: x - y
 # print(sub(5, 3))
+#
+# # iteration in a lambda function
+# lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# res = list(map(lambda x: x * 2, lst))
+# print(res)
 
-# iteration in a lambda function
-lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-res = list(map(lambda x: x * 2, lst))
-print(res)
+# Creating a Duplicate List Elements Remover Function
+
+def remove_duplicates(lst):
+    unq = list(set(lst))
+    # return unq.sort() # error: 'NoneType' object is not subscriptable // because sort() returns None
+    unq.sort()
+    return unq
+
+lst = [55, 44, 33, 22, 11, 11, 22, 33, 44, 55, 66, 77, 88, 99, 100, 100, 99, 88, 77, 66, 55, 44, 33, 22, 11]
+print(remove_duplicates(lst))
