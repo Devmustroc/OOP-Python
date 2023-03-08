2618104b1c5b7d4be52d44dde42ded0f1b12bd03
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-03-08 11:48:57 +0100

    Function

---
+++ Functions/keywords_for_Functions.py
@@ -0,0 +1,10 @@
+# Keywords for Functions
+# Python has a set of keywords that are reserved words and cannot be used as variable names, function names, or any other identifier:
+# def Function():
+#     pass
+#
+# def and(): # SyntaxError: invalid syntax
+#     pass
+#
+# def as(): # SyntaxError: invalid syntax
+#     pass
