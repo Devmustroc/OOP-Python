17086ea1bc32331c3bb83736798d5a179cab7d49
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-03-09 14:57:30 +0100

    Dictionary Comprehensions

---
+++ Object_Oriented_Programming/_str_repr.py
@@ -0,0 +1,16 @@
+# class Person():
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def __str__(self):  # __str__ is a special method that returns a string representation of an object
+#         return f'Person {self.name}, {self.age} years old.'
+#
+#     def __repr__(self):
+#         return f'Person {self.name}, {self.age}'
+#
+#
+# bob = Person('Bob', 35)
+#
+# print(bob.__str__())
+# print(bob.__repr__())
--- Object_Oriented_Programming/codeOOP.py
+++ Object_Oriented_Programming/codeOOP.py
@@ -1,23 +1,24 @@
-class Students:
-    def __init__(self, name, age, grade):
-        self.name = name
-        self.age = age
-        self.grade = grade
-
-    def get_grade(self):
-        return self.grade
-
-    def get_name(self):
-        return self.name
-
-    def average_grade(self, students):
-        total = 0
-        count = 0
-        for student in students:
-            total += student.get_grade()
-            count += 1
-        return total / count
-
-
-s1 = Students('Bob', 19, 95)
-print(s1.get_grade(), s1.get_name())
+# class Students:
+#     def __init__(self, name, age, grade):
+#         self.name = name
+#         self.age = age
+#         self.grade = grade
+#
+#     def get_grade(self):
+#         return self.grade
+#
+#     def get_name(self):
+#         return self.name
+#
+#     def average_grade(self, students):
+#         total = 0
+#         count = 0
+#         for student in students:
+#             total += student.get_grade()
+#             count += 1
+#         return total / count
+#
+#
+# s1 = Students('Bob', 19, 95)
+# print(s1.get_grade(), s1.get_name())
+#
