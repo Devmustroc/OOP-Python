b283f24408516d6db3e93c8899570abdad7b1c0f
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-03-09 11:58:32 +0100

    Dictionary Comprehensions

--- Functions/dictionary_Comprehensions.py
+++ Functions/dictionary_Comprehensions.py
@@ -10,3 +10,20 @@ user_mapping = {user[1] : user for user in users}
 # for user in users:
 #     if user == user_mapping["Bob"]:
 #         print(user)
+
+# username_input = input("Enter your username: ")
+# password_input = input("Enter your password: ")
+#
+# _, username, password = user_mapping[username_input]
+# # print(list(user_mapping[username_input]))
+# if password_input == password:
+#     print("Your details are correct!")
+# else:
+#     print("Your details are incorrect.")
+
+
+# Create a variable called student_grades. This should be a dictionary with the following keys and values:
+# 'name', 'grades', 'school'.
+# the value of each mus be  'Jose', [70, 80, 90], 'Computing'
+
+students = { 'name': 'Jose','school': 'Computing', 'grades': (66, 77, 88) }
