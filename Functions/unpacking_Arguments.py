d68bd5e89d762459c171c0abd192364834dc9327
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-03-09 12:14:46 +0100

    Dictionary Comprehensions

--- Functions/unpacking_Arguments.py
+++ Functions/unpacking_Arguments.py
@@ -5,6 +5,7 @@ def add(x, y):
 #
 # n , m = [1, 2]  # unpacking arguments list
 # print(add(n, m))
-#
+# #
 # dic1 = {'x': 1, 'y': 2}  # unpacking arguments dictionary
 # print(add(x=dic1['x'], y=dic1['y']))
+# print(add(**dic1))  # unpacking arguments dictionary
