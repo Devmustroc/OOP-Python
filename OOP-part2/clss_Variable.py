566e40c4643ceba52c1d2be8040ecd26c2c7444a
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-03-10 11:09:51 +0100

    OOP

--- OOP-part2/clss_Variable.py
+++ OOP-part2/clss_Variable.py
@@ -15,24 +15,43 @@
 # py.__init__()
 
 
-class Coder():
-    def __init__(self, name):
-        self.Name = name
+# class Coder():
+#     def __init__(self, name):
+#         self.Name = name
+#
+#     def get_lang(self, lang):
+#         self.language = lang
+#
+#     def display_lang(self):
+#         lang_str = ", ".join(self.language)
+#         print(f"I am a {lang_str} developer!")
+#
+#     def is_Pythoner(self):
+#         if "python" in self.language:
+#             print("Yes, I am a Python developer!")
+#         else:
+#             print("No, I am not a Python developer!")
+#
+# cd = Coder("jake")
+# cd.language = ["python", "java", "c++"]
+# cd.display_lang()
+# cd.is_Pythoner()
 
-    def get_lang(self, lang):
-        self.language = lang
-
-    def display_lang(self):
-        lang_str = ", ".join(self.language)
-        print(f"I am a {lang_str} developer!")
-
-    def is_Pythoner(self):
-        if "python" in self.language:
-            print("Yes, I am a Python developer!")
-        else:
-            print("No, I am not a Python developer!")
-
-cd = Coder("jake")
-cd.language = ["python", "java", "c++"]
-cd.display_lang()
-cd.is_Pythoner()
+# class Algerba():
+#     def __init__(self, r=0.0,i=0.0):
+#         self.real = r
+#         self.imag = i
+#
+#     def __add__(self, other):  # overload the + operator other is another Algerba object
+#         self.real += other.real
+#         self.imag += other.imag
+#         return self
+#
+#     def show_Value(self):
+#         print(self.real, self.imag)
+#
+#
+# num = Algerba(3.0, -4.5)
+# num1 = Algerba(2.0, 3.5)
+# num3 = num1 + num
+# num3.show_Value()
