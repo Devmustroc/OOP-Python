33bcd8989a203682f31cdbf7789213caf02b8194
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-03-10 10:34:37 +0100

    OOP

--- OOP-part2/clss_Variable.py
+++ OOP-part2/clss_Variable.py
@@ -1,9 +1,16 @@
-class Python:
-    name = 'python'
-    _age = 30
+# class Python:
+#     name = 'python'
+#     _age = 30
+#     __key = '1234'
+#
+#     def __init__(self):
+#         pass
+#
+#     def python_code(self):
+#         print("I am a python developper!")
+#
+#
+# py = Python()
+# py.python_code()
+# py.__init__()
 
-
-py= Python()
-print(Python.name)
-print(py.name)
-print(py._age)
