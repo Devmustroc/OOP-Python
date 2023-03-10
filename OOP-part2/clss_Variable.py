eabbd6d723cd51966734b8a0ec6c1ed6ffa2c9ac
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-03-10 10:47:55 +0100

    OOP

--- OOP-part2/clss_Variable.py
+++ OOP-part2/clss_Variable.py
@@ -14,3 +14,25 @@
 # py.python_code()
 # py.__init__()
 
+
+class Coder():
+    def __init__(self, name):
+        self.Name = name
+
+    def get_lang(self, lang):
+        self.language = lang
+
+    def display_lang(self):
+        lang_str = ", ".join(self.language)
+        print(f"I am a {lang_str} developer!")
+
+    def is_Pythoner(self):
+        if "python" in self.language:
+            print("Yes, I am a Python developer!")
+        else:
+            print("No, I am not a Python developer!")
+
+cd = Coder("jake")
+cd.language = ["python", "java", "c++"]
+cd.display_lang()
+cd.is_Pythoner()
