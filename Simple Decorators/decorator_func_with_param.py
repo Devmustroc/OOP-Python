ed1c34e3c598bb9faffb9d04fef8d7298608632f
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-03-10 15:02:16 +0100

    Simple decorator using panel

--- Simple Decorators/decorator_func_with_param.py
+++ Simple Decorators/decorator_func_with_param.py
@@ -1,13 +1,15 @@
 import functools
 
-user = {"username": "jose123", "access_level": "guest"}
+user = {"username": "jose123", "access_level": "billing"}
 
 
 def make_secure(func): # function that takes another function as an argument
     @functools.wraps(func)  # this is a decorator that will copy the name and docstring of the function that is passed as an argument
-    def secure_function(panel): # inner function that is returned by the make_secure function
+    def secure_function(*args, **kwargs): # inner function that is returned by the make_secure function
         if user["access_level"] == "admin":  # check if the user is an admin
-            return func(panel) # return the function that was passed as an argument
+            return func(*args, **kwargs) # return the function that was passed as an argument
+        elif user["access_level"] == "billing":
+            return func(*args, **kwargs)
         else:
             return f"No admin permission for {user['username']}." # return a message if the user is not an admin
 
@@ -19,9 +21,6 @@ def get_password(panel): # this is the same as secure_function()
     if panel == "admin":
         return "1234"
     elif panel == "billing":
-        return "super_secure_password"
-    else:
-        return "password"
-
+        return "Super Secure Password"
 
 print(get_password("billing"))  # secure_function
