5c6cdf0379f9c6669b6b6684e0ff665e117feec0
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-04-12 13:41:12 +0200

    Pandas practice with jupyter

--- projects/frace_detection/script.py
+++ projects/frace_detection/script.py
@@ -4,9 +4,13 @@ detect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 imp_img =cv2.VideoCapture('107149350-1668034438907-musk2.jpg')
 
 res, img = imp_img.read()
-# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#
-# face = detect.detectMultiScale(gray, 1.3, 5)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = detect.detectMultiScale(gray, 1.3, 5)
+
+for (x, y, w, h) in faces:
+    cv.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
 cv2.imshow('Elon Image', img)
 cv2.waitKey(0)
 imp_img.release()
